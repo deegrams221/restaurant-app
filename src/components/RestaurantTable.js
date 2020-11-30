0c77dc9a9689190717a313123abdcf61fb23fc84
Diana Grams <deegrams221@gmail.com>
2020-11-30 11:50:52 -0700

    getting table layout setup

--- src/components/RestaurantTable.js
+++ src/components/RestaurantTable.js
@@ -3,7 +3,26 @@ import React from 'react';
 export default function RestaurantTable(props) {
   return (
     <>
-      <div className='restaurant-table'></div>
+      <div className='restaurant-table'>
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>City</th>
+              <th>State</th>
+              <th>Phone Number</th>
+              <th>Genre</th>
+            </tr>
+            <tr>
+              <th>{props.name}</th>
+              <th>{props.city}</th>
+              <th>{props.state}</th>
+              <th>{props.telephone}</th>
+              <th>{props.genre}</th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
