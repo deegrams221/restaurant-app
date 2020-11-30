c374fab511e13817f2f2d37f37c6da28154a35d3
Diana Grams <deegrams221@gmail.com>
2020-11-30 09:56:26 -0700

    basic header for the app

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <>
+      <div className='header'>
+        <h1>Charter Front-End Code Challenge</h1>
+        <h2>Restaurant Table</h2>
+      </div>
+    </>
+  );
+}
