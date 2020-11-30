935f96d25bcc80fb9e8fffa7bc140587505ac41b
Diana Grams <deegrams221@gmail.com>
2020-11-30 11:58:00 -0700

    added state and useEffect with axios call, axios call is working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header';
+import RestaurantTable from './components/RestaurantTable';
 
 function App() {
+  const [tableState, setTableState] = useState([]);
+
+  // Fetch from the restaurant api
+  useEffect(() => {
+    axios
+      .get(`https://code-challenge.spectrumtoolbox.com/api/restaurants`, {
+        headers: {
+          Authorization: `Api-Key q3MNxtfep8Gt`,
+        },
+      })
+      .then((data) => {
+        console.log('data:', data);
+        setTableState(data);
+      })
+      .catch((error) => {
+        console.log('API currently down:', error);
+      });
+  }, []);
+
   return (
-    <>
-      <div className='App'>
-        <Header />
-      </div>
-    </>
+    <div className='App'>
+      <Header />
+      <RestaurantTable />
+    </div>
   );
 }
 
